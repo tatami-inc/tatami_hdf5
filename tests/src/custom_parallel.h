07195e17fe83e2f976194b6d30bb8b631f170065
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-05-18 22:34:16 -0700

    Migrated tests, actions and the doxyfile.

---
+++ tests/src/custom_parallel.h
@@ -0,0 +1,35 @@
+#ifndef HDF5_CUSTOM_LOCK_H
+#define HDF5_CUSTOM_LOCK_H
+
+template<class Function>
+static void hdf5_serialize(Function f) {
+    static std::mutex hdf5_lock;
+    std::lock_guard<std::mutex> thing(hdf5_lock);
+    f();
+}
+
+template<class Function_>
+static void custom_parallelize(Function_ f, size_t ntasks, size_t nworkers) {
+    size_t start = 0;
+    std::vector<std::thread> jobs;
+    jobs.reserve(nworkers);
+    size_t jobs_per_worker = std::ceil(static_cast<double>(ntasks) / static_cast<double>(nworkers));
+
+    for (size_t w = 0; w < nworkers; ++w) {
+        size_t end = std::min(ntasks, start + jobs_per_worker);
+        if (start >= end) {
+            break;
+        }
+        jobs.emplace_back(f, w, start, end - start);
+        start += jobs_per_worker;
+    }
+
+    for (auto& job : jobs) {
+        job.join();
+    }
+}
+
+#define TATAMI_CUSTOM_PARALLEL custom_parallelize
+#define TATAMI_HDF5_PARALLEL_LOCK hdf5_serialize
+
+#endif
