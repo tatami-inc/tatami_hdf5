6a7e26cc1a258f65f16e47ad7702e268a94bf665
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-08-26 20:30:28 +0000

    Deploying to gh-pages from @ tatami-inc/tatami_hdf5@693c3c59029860d705d3fe8766f010a903bee272 🚀

--- docs/search/all_e.js
+++ docs/search/all_e.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['reference_0',['reference',['https://tatami-inc.github.io/tatami/structtatami_1_1SomeNumericArray_1_1Iterator.html#a4bf2843ca912061c3cc0400bbb75abe6',1,'tatami::SomeNumericArray::Iterator']]],
   ['require_5fminimum_5fcache_1',['require_minimum_cache',['../structtatami__hdf5_1_1DenseMatrixOptions.html#a5d24e3dc75b665cb8a54d3d2d1578afb',1,'tatami_hdf5::DenseMatrixOptions']]],
-  ['retrieve_5fcompressed_5fsparse_5fcontents_2',['retrieve_compressed_sparse_contents',['https://tatami-inc.github.io/tatami/namespacetatami.html#ad723a47ee2e03e4213cd07fda8710e65',1,'tatami']]],
+  ['retrieve_5fcompressed_5fsparse_5fcontents_2',['retrieve_compressed_sparse_contents',['https://tatami-inc.github.io/tatami/namespacetatami.html#a6b95eab5d7370f965e4d6a9b9a9f3393',1,'tatami']]],
   ['retrieve_5ffragmented_5fsparse_5fcontents_3',['retrieve_fragmented_sparse_contents',['https://tatami-inc.github.io/tatami/namespacetatami.html#ab5688af2360372f1535d6d68b2c14de6',1,'tatami']]]
 ];
--- docs/search/variables_6.js
+++ docs/search/variables_6.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['pointers_0',['pointers',['https://tatami-inc.github.io/tatami/structtatami_1_1CompressedSparseContents.html#ad56fc74d757e9dcd8ffbdc48d5adc692',1,'tatami::CompressedSparseContents']]],
+  ['pointers_0',['pointers',['https://tatami-inc.github.io/tatami/structtatami_1_1CompressedSparseContents.html#ad9410e8d3b32bc97086bebf2322c0ef8',1,'tatami::CompressedSparseContents']]],
   ['ptr_5fname_1',['ptr_name',['../structtatami__hdf5_1_1WriteCompressedSparseMatrixOptions.html#a02bcb589398f16c0b52621bfeae814d3',1,'tatami_hdf5::WriteCompressedSparseMatrixOptions']]]
 ];
