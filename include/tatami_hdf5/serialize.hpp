ed73085357c3f6d49809bb28947b3fbf60780fa2
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-08-26 13:28:24 -0700

    Use the SUBPAR_USES_OPENMP macro when choosing the lock method.
    This is motivated by tatami's switch to using the subpar library for
    its parallelization, which auto-chooses between OpenMP and <thread>.
    By checking the macro, we make sure that we use a locking scheme that is
    appropriate to the parallelization scheme chosen by tatami::parallelize,
    a.k.a., the new subpar::parallelize. (Unless overridden by macros, in
    which case all bets are off and the developer should figure it out.)
    
    Also avoid include's of the standard headers inside our own namespace.

--- include/tatami_hdf5/serialize.hpp
+++ include/tatami_hdf5/serialize.hpp
@@ -6,19 +6,19 @@
  * @brief Default locking for serial access.
  */
 
-namespace tatami_hdf5 {
-
 /**
  * @cond
  */
 #ifndef TATAMI_HDF5_PARALLEL_LOCK
-#ifndef _OPENMP
+#include "subpar/subpar.hpp"
+#ifndef SUBPAR_USES_OPENMP
 #include <thread>
 #include <mutex>
-
+namespace tatami_hdf5 {
 inline auto& get_default_hdf5_lock() {
     static std::mutex hdf5_lock;
     return hdf5_lock;
+}
 }
 #endif
 #endif
@@ -26,34 +26,36 @@ inline auto& get_default_hdf5_lock() {
  * @endcond
  */
 
+namespace tatami_hdf5 {
+
 /**
  * Serialize a function's execution to avoid simultaneous calls to the (non-thread-safe) HDF5 library.
+ * This is primarily intended for use inside `tatami::parallelize()` but can also be called anywhere that uses the same parallelization scheme.
+ * Also check out the [**subpar**](https://ltla.github.io/subpar) library, which implements the default parallelization scheme for `tatami::parallelize()`.
  *
- * If the `TATAMI_HDF5_PARALLEL_LOCK` macro is defined, it should be a function-like macro that accepts `f` and executes it in a serial section.
+ * The default serialization mechanism is automatically determined from the definition of the `SUBPAR_USES_OPENMP` macro.
+ * If defined (i.e., OpenMP is used), `f` is executed in OpenMP critical regions named `"hdf5"`.
+ * Otherwise, a global mutex from `<mutex>` is used to guard the execution of `f`.
  *
- * If OpenMP is available, serialization is achieved by running `f` inside OpenMP critical regions named `"hdf5"`.
- *
- * Otherwise, we use a mutex from the standard `<thread>` library to guard the execution of `f`.
+ * If a custom parallelization scheme is defined via `TATAMI_CUSTOM_PARALLEL` or `SUBPAR_CUSTOM_PARALLEL`, the default serialization mechanism may not be appropriate.
+ * Users should instead define a `TATAMI_HDF5_PARALLEL_LOCK` function-like macro that accepts `f` and executes it in a serial section appropriate to the custom scheme.
+ * Once defined, this user-defined lock will be used in all calls to `serialize()`.
  *
  * @param f Function to be run in a serial section.
  * This accepts no arguments and returns no outputs.
- *
  */
 template<class Function_>
 void serialize(Function_ f) {
 #ifdef TATAMI_HDF5_PARALLEL_LOCK
     TATAMI_HDF5_PARALLEL_LOCK(f);
-#else
-#ifdef _OPENMP
+#elif defined(SUBPAR_USES_OPENMP)
     #pragma omp critical(hdf5)
     {
         f();
     }
 #else
-    static std::mutex hdf5_lock;
     std::lock_guard<std::mutex> thing(get_hdf5_lock());
     f();
-#endif
 #endif
 }
 
