d93262eb8d99731be3348bd61521eca403c770ea
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-08-30 09:52:15 -0700

    Switch to the new SUBPAR_USES_OPENMP_RANGE macro in subpar v0.3.1.

--- include/tatami_hdf5/serialize.hpp
+++ include/tatami_hdf5/serialize.hpp
@@ -8,7 +8,7 @@
 
 #ifndef TATAMI_HDF5_PARALLEL_LOCK
 #include "subpar/subpar.hpp"
-#ifndef SUBPAR_USES_OPENMP
+#ifndef SUBPAR_USES_OPENMP_RANGE
 #include <mutex>
 #include <thread>
 namespace tatami_hdf5 {
@@ -30,11 +30,11 @@ namespace tatami_hdf5 {
  * This is primarily intended for use inside `tatami::parallelize()` but can also be called anywhere that uses the same parallelization scheme.
  * Also check out the [**subpar**](https://ltla.github.io/subpar) library, which implements the default parallelization scheme for `tatami::parallelize()`.
  *
- * The default serialization mechanism is automatically determined from the definition of the `SUBPAR_USES_OPENMP` macro.
+ * The default serialization mechanism is automatically determined from the definition of the `SUBPAR_USES_OPENMP_RANGE` macro.
  * If defined (i.e., OpenMP is used), `f` is executed in OpenMP critical regions named `"hdf5"`.
  * Otherwise, a global mutex from `<mutex>` is used to guard the execution of `f`.
  *
- * If a custom parallelization scheme is defined via `TATAMI_CUSTOM_PARALLEL` or `SUBPAR_CUSTOM_PARALLEL`, the default serialization mechanism may not be appropriate.
+ * If a custom parallelization scheme is defined via `TATAMI_CUSTOM_PARALLEL` or `SUBPAR_CUSTOM_PARALLELIZE_RANGE`, the default serialization mechanism may not be appropriate.
  * Users should instead define a `TATAMI_HDF5_PARALLEL_LOCK` function-like macro that accepts `f` and executes it in a serial section appropriate to the custom scheme.
  * Once defined, this user-defined lock will be used in all calls to `serialize()`.
  *
@@ -46,7 +46,7 @@ void serialize(Function_ f) {
 #ifdef TATAMI_HDF5_PARALLEL_LOCK
     TATAMI_HDF5_PARALLEL_LOCK(f);
 #else
-#ifdef SUBPAR_USES_OPENMP
+#ifdef SUBPAR_USES_OPENMP_RANGE
     #pragma omp critical(hdf5)
     {
         f();
