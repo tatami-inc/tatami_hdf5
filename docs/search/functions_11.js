28f425a8327b150d5e6074d7e9f29574c7a4797e
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2025-10-06 18:19:29 +0000

    Deploying to gh-pages from @ tatami-inc/tatami_hdf5@9edcfcb26cee4f19cbcb2ae22054908f4022f24f 🚀

--- docs/search/functions_11.js
+++ docs/search/functions_11.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['wrap_5fshared_5fptr_0',['wrap_shared_ptr',['https://tatami-inc.github.io/tatami/namespacetatami.html#ab008bae4e33a408de29e9fc49d0f902c',1,'tatami']]],
+  ['wrap_5fshared_5fptr_0',['wrap_shared_ptr',['https://tatami-inc.github.io/tatami/namespacetatami.html#a5a07f7937bfdbbe8e355cba20d7c0c3a',1,'tatami']]],
   ['write_5fcompressed_5fsparse_5fmatrix_1',['write_compressed_sparse_matrix',['../namespacetatami__hdf5.html#abbb2c7427230171df09b7404f3931e02',1,'tatami_hdf5::write_compressed_sparse_matrix(const tatami::Matrix&lt; Value_, Index_ &gt; *mat, H5::Group &amp;location, const WriteCompressedSparseMatrixOptions &amp;params)'],['../namespacetatami__hdf5.html#a8534a867ece6f241802d5cabc9b19d13',1,'tatami_hdf5::write_compressed_sparse_matrix(const tatami::Matrix&lt; Value_, Index_ &gt; *mat, H5::Group &amp;location)']]]
 ];
